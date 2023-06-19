73a799410b4cc8f804814c7aeeb1cbc7ed8ee42e
Traveler <laiqw6537@qq.com>
2023-06-19 19:06:43 +0800

    批量编写HTML链接

---
+++ C语言简单练习/批量输出HTML链接.c
@@ -0,0 +1,17 @@
+/*
+	Name:
+	Copyright:劣等生
+	Author:劣等生
+	Date:
+	Description:
+*/
+#include<stdio.h>
+
+int main(){
+	int i;
+    for(i = 0;i < 147;i++){
+		printf("<a href=\".\\女儿闺蜜都归ME\\第%d话.mht\">第%d话</a>\n",i + 1,i + 1);
+	}
+	return 0;
+}
+
